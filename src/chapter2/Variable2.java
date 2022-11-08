d40d850c8dfad26a700621f173ffb201a1b12805
flypigmong <jseonghun85@gmail.com>
2022-11-08 23:37:15 +0900

    문자형 연습

--- src/chapter2/Variable2.java
+++ src/chapter2/Variable2.java
@@ -4,7 +4,7 @@ package chapter2;
 public class Variable2 {
 	public static void main(String[] args) {
 		int level=10;
-		System.out.println(level); //level 변수 선언과 동시에 대입(초기화
+		System.out.println(level); //level 변수 선언과 동시에 대입(초기화)
 	}
 
 }
