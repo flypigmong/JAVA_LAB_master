39195f643540099bfe128e65363c5dc47b8eeb06
flypigmong <jseonghun85@gmail.com>
2022-11-08 23:43:51 +0900

    실수형 연습

---
+++ src/chapter2/DoubleEx1.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+//실수형 연습
+public class DoubleEx1 {
+	public static void main(String[] args) {
+		double dnum = 3.14;
+		float fnum = 3.14F; //Long의 식별자인 L,l과 비슷
+		
+		System.out.println(dnum);
+		System.out.println(fnum);
+	}
+}
