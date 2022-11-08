9473687ce9592e3b80072508095a45f8c024015a
flypigmong <jseonghun85@gmail.com>
2022-11-08 23:17:41 +0900

    주석 달기

--- src/chapter2/Variable2.java
+++ src/chapter2/Variable2.java
@@ -1,11 +1,10 @@
 package chapter2;
 
-public class Variable2 {
 //변수 초기화하기
+public class Variable2 {
 	public static void main(String[] args) {
 		int level=10;
-		System.out.println(level);
-
+		System.out.println(level); //level 변수 선언과 동시에 대입(초기화
 	}
 
 }
